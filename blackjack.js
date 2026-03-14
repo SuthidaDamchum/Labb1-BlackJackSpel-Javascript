74c3fe1da7d60a01d976562ba46ac10493d37071
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-03-14 12:44:25 +0100

    Created buildDeck function to generate 52 cards

---
+++ blackjack.js
@@ -0,0 +1,36 @@
+var hiddenCard;
+
+window.onload = function () {
+  buildDeck();
+
+};
+
+
+
+let deck = []
+function buildDeck() {
+  let values = [
+    "ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "jack",
+    "queen",
+    "king",
+  ];
+  let types = ["clubs", "diamonds", "hearts", "spades"];
+
+  for (let i = 0; i < types.length; i++) {
+    for (let j = 0; j < values.length; j++) {
+      let card = values[j] + "_of_" + types[i];
+      deck.push(card);
+    }
+  }
+  console.log(deck);
+}

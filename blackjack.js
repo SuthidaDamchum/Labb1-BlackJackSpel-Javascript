9183485b8b0a98d32b33cab9cf6c19576c40396e
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-03-16 14:04:34 +0100

    Fixed display card images and  scores on UI

--- blackjack.js
+++ blackjack.js
@@ -1,15 +1,17 @@
 var playerSum = 0;
 var dealerSum = 0;
-var playerAceCount = 0;
-var dealerAceCount = 0;
+var playerAcesCount = 0;
+var dealerAcesCount = 0;
 var hiddenCard;
 
 window.onload = function () {
   buildDeck();
   shuffleDeck();
+  startGame();
 };
 
 let deck = [];
+
 function buildDeck() {
   let values = [
     "ace",
@@ -26,6 +28,7 @@ function buildDeck() {
     "queen",
     "king",
   ];
+
   let types = ["clubs", "diamonds", "hearts", "spades"];
 
   for (let i = 0; i < types.length; i++) {
@@ -48,13 +51,37 @@ function shuffleDeck() {
 }
 
 function startGame() {
+  //Hiiden kort value med första korten
+  console.log("Nu startar spelet!");
   hiddenCard = deck.pop();
   dealerSum += getCardValue(hiddenCard);
-  dealerAceCount += checkAcs(card)
-  
+  dealerAcesCount += checkAces(hiddenCard);
+
+  //Andra
   let card = deck.pop();
-  playerSum += getCardValue(value)
-  dealerAceCount += checkAcs(card)
+  let cardImage = document.createElement("img");
+  cardImage.src = "./cards/" + card + ".png";
+  dealerSum += getCardValue(card);
+  dealerAcesCount += checkAces(card);
+  document.getElementById("DealerCards").append(cardImage);
+  document.getElementById("dealerSum").innerText = dealerSum;
+
+  //Player 1
+  card = deck.pop();
+  cardImage = document.createElement("img");
+  cardImage.src = "./cards/" + card + ".png";
+  playerSum += getCardValue(card);
+  playerAcesCount += checkAces(card);
+  document.getElementById("PlayerCards").append(cardImage);
+
+  //kort 2
+  card = deck.pop();
+  cardImage = document.createElement("img");
+  cardImage.src = "./cards/" + card + ".png";
+  playerSum += getCardValue(card);
+  playerAcesCount += checkAces(card);
+  document.getElementById("PlayerCards").append(cardImage);
+  document.getElementById("playerSum").innerText = playerSum;
 }
 
 function getCardValue(card) {
@@ -62,23 +89,37 @@ function getCardValue(card) {
   let value = splitCard[0];
 
   if (isNaN(value)) {
-    if (value == "ace") {
+    if (value === "ace") {
       return 11;
     }
     return 10;
   }
   return parseInt(value);
+}
 
-
-function checkAcs(){
-
+function checkAces(card) {
   let splitCard = card.split("_of_");
-  let value = splitCard[0]
+  let value = splitCard[0];
 
-  if (value === "ace"){
-        return 1;
-      }
-        return 0; 
+  if (value === "ace") {
+    return 1;
+  }
+  return 0;
 }
-   
+
+let message = "";
+
+if (playerSum > 21) {
+  message = "Dealer Wins!";
+} else if (dealerSum > 21) {
+  message = "You Win!";
+} else if (playerSum == dealerSum) {
+  message = "Tie!";
+} else if (playerSum > dealerSum) {
+  message = "You Win!";
+} else {
+  message = "Dealer Wins!";
 }
+doucment.getElementById("results").innerText = message;
+
+function moreCard(card) {}
